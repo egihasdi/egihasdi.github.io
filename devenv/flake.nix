3afe8ec44f474e8c914e1d3410e7869a795bd394
egi.hasdi <egi.hasdi@gmail.com>
2025-01-23 14:07:48 +0700

    update deps

--- devenv/flake.nix
+++ devenv/flake.nix
@@ -7,73 +7,35 @@
 
     devenv.url = "github:cachix/devenv";
     devenv.inputs.nixpkgs.follows = "nixpkgs";
-
-    nixvim.url = "github:nix-community/nixvim";
-    nixvim.inputs.nixpkgs.follows = "nixpkgs-unstable";
   };
 
   nixConfig = {
-    extra-trusted-public-keys = "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
+    extra-trusted-public-keys =
+      "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
     extra-substituters = "https://devenv.cachix.org";
   };
 
-  outputs =
-    {
-      self,
-      nixpkgs,
-      nixpkgs-unstable,
-      devenv,
-      systems,
-      nixvim,
-      ...
-    }@inputs:
-    let
-      forEachSystem = nixpkgs.lib.genAttrs (import systems);
-    in
-    {
+  outputs = { self, nixpkgs, nixpkgs-unstable, devenv, systems, ... }@inputs:
+    let forEachSystem = nixpkgs.lib.genAttrs (import systems);
+    in {
       packages = forEachSystem (system: {
         devenv-up = self.devShells.${system}.default.config.procfileScript;
       });
 
-      devShells = forEachSystem (
-        system:
-        let
-          pkgs = nixpkgs.legacyPackages.${system};
-
-          pkgs-unstable = nixpkgs-unstable.legacyPackages.${system};
-          nixvim' = nixvim.legacyPackages.${system};
-        in
-        {
+      devShells = forEachSystem (system:
+        let pkgs = nixpkgs.legacyPackages.${system};
+        in {
           default = devenv.lib.mkShell {
             inherit inputs pkgs;
             modules = [
-              (
-                { config, ... }:
-                let
-                  nixvimModule = {
-                    pkgs = pkgs-unstable;
-                    module = import ./nixvim;
-                    extraSpecialArgs =
-                      {
-                        root = config.devenv.root;
-                      };
-                  };
-                  nvim = nixvim'.makeNixvimWithModule nixvimModule;
-                in
-                {
-                  # https://devenv.sh/reference/options/
-                  packages = [
-                    nvim
-                    pkgs.xclip
-                    pkgs.ripgrep
-                  ];
-                  
-                  languages.deno.enable = true;
-                }
-              )
+              ({ config, ... }: {
+                # https://devenv.sh/reference/options/
+                packages = [ pkgs.xclip pkgs.ripgrep ];
+
+                languages.deno.enable = true;
+              })
             ];
           };
-        }
-      );
+        });
     };
 }
